5bc141cde04e910160c4bcb53bb61e6800d5e0ee
eloguidici <emilianologuidici@gmail.com>
2026-09-01 22:47:39 -0300

    feat: fail fast on invalid configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HealthController } from './common/health/health.controller';
+import { validateEnvironment } from './config/environment';
 import { RagModule } from './rag/rag.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), RagModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: validateEnvironment,
+    }),
+    RagModule,
+  ],
   controllers: [HealthController],
 })
 export class AppModule {}

04dd36e0c0de931c2e9cd3398a7937bf82e869cd
eloguidici <emilianologuidici@gmail.com>
2026-09-01 21:46:18 -0300

    feat: register health controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { HealthController } from './common/health/health.controller';
 import { RagModule } from './rag/rag.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), RagModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
